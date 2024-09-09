43f58f4ae7582b4711690154d42c6f18ce660bf7
ivan-puhachov <tiltoslave@gmail.com>
2024-09-09 19:51:58 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VikaVisual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "Bunny";
+            label2.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            label2.Text = "Jänes";
+            label1.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+            label3.Text = "Заяц";
+            label2.Visible = false;
+            label1.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
